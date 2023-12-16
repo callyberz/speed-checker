87c4ca3997dac6227cbb982290ea386f438bf0ad
hkclki <hokheil26@gmail.com>
2023-12-16 15:37:29 -0500

    [FIX] history.push not showing up playback rate properly

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,32 +1,23 @@
 import { StorageUtils } from '@src/utils/StorageUtils';
 import './style.css';
 
-function render() {
-  const div = document.createElement('div');
-  div.id = '__root';
-  document.body.appendChild(div);
-
-  const videoElement = document.querySelector('video');
-  if (!videoElement) {
-    console.error('video element not found');
+function renderSpeedDisplay(videoElement: HTMLVideoElement) {
+  const speedDisplay = document.getElementById('speedDisplay');
+  if (speedDisplay) {
     return;
   }
 
-  // Create a custom div element for displaying the speed
   const customDiv = document.createElement('div');
   customDiv.style.position = 'absolute';
   customDiv.style.top = '0';
   customDiv.style.left = '0';
   customDiv.style.zIndex = '50';
-  customDiv.id = 'speedDisplay'; // Set an ID for easy reference
+  customDiv.id = 'speedDisplay';
   customDiv.innerText = `Your current speed is: ${StorageUtils.getSpeed()}x`;
 
-  // Append the custom div to the videoElement's parent
   videoElement.parentNode?.appendChild(customDiv);
 
-  // Listen for the 'ratechange' event on the video element
   videoElement.addEventListener('ratechange', function () {
-    // Update the displayed speed when the playback speed changes
     const speedDisplay = document.getElementById('speedDisplay');
     if (speedDisplay) {
       speedDisplay.innerText = `Your current speed is: ${videoElement.playbackRate}x`;
@@ -34,57 +25,36 @@ function render() {
   });
 }
 
-function playbackRateInit() {
-  // Create a MutationObserver instance
+function observeVideoChanges() {
   const observer = new MutationObserver((mutationsList) => {
     for (const mutation of mutationsList) {
-      // Check if the mutation involves attributes or properties of the video element
-      if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
-        // Video source changed, reattach the playback rate listener
-        attachPlaybackRateListener();
+      if (
+        mutation.type === 'attributes' &&
+        (mutation.attributeName === 'src' || mutation.attributeName === 'href')
+      ) {
+        const videoElement = document.querySelector('video');
+        if (videoElement) {
+          renderSpeedDisplay(videoElement);
+        }
       }
     }
   });
 
-  // Function to monitor changes in playbackRate
-  function attachPlaybackRateListener() {
-    const video = document.querySelector('video');
-
-    if (video) {
-      const rateChangeHandler = function () {
-        console.log('Playback rate changed:', video.playbackRate);
-        localStorage.setItem('speed', video.playbackRate.toString());
-      };
-
-      video.addEventListener('ratechange', rateChangeHandler);
-    }
-  }
-
-  // Start observing changes in the video element
-  const targetVideo = document.querySelector('video');
-  if (targetVideo) {
-    attachPlaybackRateListener();
-    observer.observe(targetVideo, { attributes: true });
-  }
+  const targetNode = document.documentElement;
+  observer.observe(targetNode, { subtree: true, attributes: true });
 }
 
-// Monitor URL changes and send a message to the background script
-function checkURLChange() {
-  const newURL = window.location.href;
-  if (newURL !== currentURL) {
-    currentURL = newURL;
-    render();
+function initializeExtension() {
+  observeVideoChanges();
+  const videoElement = document.querySelector('video');
+  if (videoElement) {
+    renderSpeedDisplay(videoElement);
   }
 }
-
-// Set up interval to check for URL changes
-let currentURL = window.location.href;
-setInterval(checkURLChange, 500);
 
 try {
-  playbackRateInit();
-  render();
-  console.log('rendered');
+  initializeExtension();
+  console.log('Extension initialized');
 } catch (e) {
-  console.error(e);
+  console.error('Error initializing extension:', e);
 }
